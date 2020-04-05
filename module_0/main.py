d25184d0bb18c4f80684e0da2087e57492bb531c
Azarodnyuk <Alenaz_90@inbox.ru>
2020-04-05 18:08:00 +0300

    Advanced guessing main.py

--- module_0/main.py
+++ module_0/main.py
@@ -1,46 +1,35 @@
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-from scipy import integrate
 
-def solve_lorenz(sigma=10.0, beta=8./3, rho=28.0):
-    """Plot a solution to the Lorenz differential equations."""
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом 
+       уменьшаем или увеличиваем его в зависимости от того, 
+       больше оно или меньше нужного. Функция принимает 
+       загаданное число и возвращает число попыток'''
+    count = 1
+    a=1
+    b=101
+    predict = np.random.randint(a,b)
+    while number != predict:
+        count+=1
+        if number > predict: 
+            a = predict + 1
+            predict = np.random.randint(a,b)
+        elif number < predict: 
+            b = predict
+            predict = np.random.randint(a,b)
+    return(count) # выход из цикла, если угадали
 
-    max_time = 4.0
-    N = 30
+def score_game(game_core_v1):
+    '''Запускаем игру 1000 раз, чтобы узнать, 
+       как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core_v1(number))
+    score = int(np.mean(count_ls))
+    print(f"Алгоритм угадывает число в среднем за {score} попыток")
+#     return(score)
 
-    fig = plt.figure()
-    ax = fig.add_axes([0, 0, 1, 1], projection='3d')
-    ax.axis('off')
-
-    # prepare the axes limits
-    ax.set_xlim((-25, 25))
-    ax.set_ylim((-35, 35))
-    ax.set_zlim((5, 55))
-    
-    def lorenz_deriv(x_y_z, t0, sigma=sigma, beta=beta, rho=rho):
-        """Compute the time-derivative of a Lorenz system."""
-        x, y, z = x_y_z
-        return [sigma * (y - x), x * (rho - z) - y, x * y - beta * z]
-
-    # Choose random starting points, uniformly distributed from -15 to 15
-    np.random.seed(1)
-    x0 = -15 + 30 * np.random.random((N, 3))
-
-    # Solve for the trajectories
-    t = np.linspace(0, max_time, int(250*max_time))
-    x_t = np.asarray([integrate.odeint(lorenz_deriv, x0i, t)
-                      for x0i in x0])
-    
-    # choose a different color for each trajectory
-    colors = plt.cm.viridis(np.linspace(0, 1, N))
-
-    for i in range(N):
-        x, y, z = x_t[i,:,:].T
-        lines = ax.plot(x, y, z, '-', c=colors[i])
-        plt.setp(lines, linewidth=2)
-    angle = 104
-    ax.view_init(30, angle)
-    plt.show()
-
-    return t, x_t
+# Проверяем
+score_game(game_core_v2)
