d43a088a3000d335b471f8f8154509a1fa65fc2b
Alena <57530983+Azarodnyuk@users.noreply.github.com>
2020-07-10 19:34:41 +0300

    Add files via upload

--- module_2/functions_module_2.py
+++ module_2/functions_module_2.py
@@ -3,6 +3,13 @@
 
 # In[1]:
 
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+from itertools import combinations
+from scipy.stats import ttest_ind
+
+
 
 def description():
     
@@ -70,8 +77,131 @@ def description():
     return pd.DataFrame(data=data, index=index, columns = ['description'])
 
 
-# In[ ]:
+
+
+def scatter_statistics(math):
+    
+    quantative_columns = math.select_dtypes(exclude='object').columns
+    
+    fig, axes = plt.subplots(3,4, figsize=(19,13))
+
+    for j in range(4):
+        for i in range(3):
+
+            col = quantative_columns[4*i+j]
+
+            group_max = math.groupby(col,sort=True,axis=0).max().score
+            group_min = math.groupby(col,sort=True,axis=0).min().score
+            group_median = math.groupby(col,sort=True,axis=0).median().score
 
 
 
+            axes[i][j].scatter(data=math, x=col, y=quantative_columns[12],marker='.')
+
+            axes[i][j].plot(list(group_max.index), 
+                         group_max, '-o',label='max')
+
+            axes[i][j].plot(list(group_min.index), 
+                         group_min, '-o', label='min')
+
+            axes[i][j].plot(list(group_median.index), 
+                         group_median, '-o', label='median')
+
+            axes[i][j].grid(True)
+            axes[i][j].set_xlabel(col)
+            axes[i][j].set_ylabel('score')
+            axes[i][j].legend()
+
+            
+            
+            
+def investigate_num_col(math, col, math_description):
+    
+    fig, axes = plt.subplots(1,2, figsize=(15,3))
+    
+    if col != 'studytime, granular':
+        print('\n*********************************************************************')
+        print(col,'-',math_description.loc[col,'description'])
+        print('\n*********************************************************************')
+        
+    else:        
+        print('\n*********************************************************************')
+        print(col,'-  no description')
+        print('\n*********************************************************************')
+        
+    print('\nПосчитаем количество каждого из значений в столбце', col)
+    data = pd.DataFrame(data=math[col].value_counts(ascending=False))
+    data.columns = ['counts']
+    display(data.T)
+
+    print('\nРассмотрим статистики в столбце', col)
+    display(pd.DataFrame(data=math.describe()[col]).T)
+
+    sns.distplot(math[col], rug=True, bins=len(math[col].value_counts()), ax=axes[0])
+    axes[0].set_title('\nГистограмма распределения \n значений столбца {}\n'.format(col))
+    axes[0].grid(True)
+
+    sns.boxplot(y=col,  data=math, ax=axes[1], color='orange', orient='h', saturation=1.0)
+    axes[1].set_title('\n Boxplot for ' + col +'\n')
+    axes[1].grid(True)
+        
+        
+        
+        
+def get_boxplot(data, column):
+    fig, ax = plt.subplots(figsize = (8, 3))
+    sns.boxplot(x=column, 
+                y='score', 
+                data=data,
+                ax=ax)
+#     plt.xticks(rotation=45)
+    ax.set_title('Boxplot for ' + column)
+    plt.show()
+    
+    
+    
+    
+def get_stat_dif(data, column):
+    cols = data.loc[:, column].value_counts().index
+    combinations_all = list(combinations(cols, 2))
+    for comb in combinations_all:
+        if ttest_ind(data.loc[data.loc[:, column] == comb[0], 'score'], 
+                        data.loc[data.loc[:, column] == comb[1], 'score']).pvalue \
+            <= 0.05/len(combinations_all): # Учли поправку Бонферони
+            print('Найдены статистически значимые различия для колонки:', column)
+            break
+            
+            
+            
+            
+def null_count(data):
+    null_count = pd.DataFrame(index=data.columns, columns=['null_count'])
+    for i in list(null_count.index):
+        null_count.loc[i,'null_count'] = data[i].isna().sum()
+    null_count['null_count_percent'] = null_count['null_count']*100 / len(data)
+    return null_count
+
+
+
+
+def pairplot(data, start_x, stop_x, start_y,stop_y):
+    
+    """This function let us to make pairplot for choosen range of quantative columns"""
+    
+    quantative_columns = data.select_dtypes(exclude='object').columns
+    sns.pairplot(data, 
+                 x_vars=quantative_columns[start_x:stop_x],
+                 y_vars=quantative_columns[start_y:stop_y], 
+                 kind = 'reg', height=2, markers='.');
+
+
+    
+    
+    
+def statistics(data1,data2, col):
+    alpha = ttest_ind(data1[col], data2[col], equal_var=False, nan_policy='omit').pvalue 
+    if alpha < 0.05:
+        print("\n Выборочные средние по столбцу",col, "не равны")
+#     else:
+#         print("\n Выборочные средние по столбцу",col, "возможно равны")
 
